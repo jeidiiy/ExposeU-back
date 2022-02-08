bf28660bc7b1aa45adb1b58e022eb2466eee6b4d
Ji-Yong, Jeong <wldyd2356@gmail.com>
2022-02-08 20:44:29 +0900

    init: 프로젝트 시작

---
+++ src/main/kotlin/wscrg/exposeuback/ExposeUBackApplication.kt
@@ -0,0 +1,11 @@
+package wscrg.exposeuback
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ExposeUBackApplication
+
+fun main(args: Array<String>) {
+    runApplication<ExposeUBackApplication>(*args)
+}
---
+++ src/test/kotlin/wscrg/exposeuback/ExposeUBackApplicationTests.kt
@@ -0,0 +1,13 @@
+package wscrg.exposeuback
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ExposeUBackApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
