ab84c596504fb3163049b82bbf2b08814f4daef3
wscrg <wldyd2356@gmail.com>
2022-03-16 23:43:22 +0900

    refactor: @DisplayName 적용 및 코드 정리

--- src/test/kotlin/wscrg/exposeuback/api/user/controller/UserApiControllerTest.kt
+++ src/test/kotlin/wscrg/exposeuback/api/user/controller/UserApiControllerTest.kt
@@ -2,65 +2,54 @@ package wscrg.exposeuback.api.user.controller
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
-import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.http.MediaType
-import org.springframework.mock.web.MockMultipartFile
 import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import org.springframework.transaction.annotation.Transactional
-import wscrg.exposeuback.domain.dto.user.UserForm
-import java.util.UUID
+import wscrg.exposeuback.createMockUser
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
 internal class UserApiControllerTest private constructor(
-    @Autowired
-    val mockMvc: MockMvc,
+    @Autowired val mockMvc: MockMvc,
 ) {
-    companion object {
-        val log = LoggerFactory.getLogger(MockMvcResultMatchers::class.java)
-    }
-
     val objectMapper = ObjectMapper().registerKotlinModule()
 
     @Test
-    fun user_and_image_동시_업로드_테스트() {
+    @DisplayName("유저 및 이미지 정보를 multipart/form-data로 함께 제출")
+    fun signUp() {
         //given
         val requestBuilder = createMockUser(hasImage = true)
 
         //when
-        val perform = mockMvc.perform(requestBuilder)
-
-        //then
-        val result = perform.andExpect(status().isCreated).andReturn()
-        val response = result.response
-        log.info("response content-type: {}", response.contentType)
-        log.info("response content: {}", response.contentAsString)
+        mockMvc.perform(requestBuilder)
+            //then
+            .andExpect(status().isCreated).andReturn()
     }
 
     @Test
-    fun image_미업로드시_400_응답() {
+    @DisplayName("이미지 미업로드 시 응답코드 400")
+    fun imageIsNotUploaded() {
         //given
         //file-data 없음
         val requestBuilder = createMockUser(hasImage = false)
 
         //when
-        val perform = mockMvc.perform(requestBuilder)
-
-        //then
-        perform.andExpect(status().isBadRequest)
+        mockMvc.perform(requestBuilder)
+            //then
+            .andExpect(status().isBadRequest)
     }
 
     @Test
-    fun 회원조회() {
+    @DisplayName("회원조회")
+    fun selectUser() {
         //given
         val requestBuilder = createMockUser(hasImage = true)
 
@@ -69,16 +58,16 @@ internal class UserApiControllerTest private constructor(
         val userResponseDto = objectMapper.readValue(mvcResult.response.contentAsString, UserResponseDto::class.java)
 
         //when
-        val resultActions = mockMvc.perform(
+        mockMvc.perform(
             get("/api/users/${userResponseDto.id}")
         )
-
-        //then
-        resultActions.andExpect(status().isOk)
+            //then
+            .andExpect(status().isOk)
     }
 
     @Test
-    fun 회원탈퇴() {
+    @DisplayName("회원탈퇴")
+    fun dropOut() {
         //given
         val requestBuilder = createMockUser(hasImage = true)
 
@@ -90,53 +79,11 @@ internal class UserApiControllerTest private constructor(
         jsonData["id"] = userResponseDto.id
 
         //when
-        val resultActions = mockMvc.perform(
+        mockMvc.perform(
             delete("/api/users/${userResponseDto.id}")
         )
-
-        //then
-        resultActions.andExpect(status().isNoContent)
-    }
-
-    fun createMockUser(hasImage: Boolean): MockHttpServletRequestBuilder {
-        val imageFile = if (hasImage)
-            MockMultipartFile(
-                "file-data",
-                "image.jpeg",
-                MediaType.IMAGE_JPEG_VALUE,
-                "<<image jpeg>>".toByteArray()
-            ) else null
-
-        var random = UUID.randomUUID().toString()
-        random = random.substring(random.lastIndexOf("-") + 1)
-
-        val content =
-            objectMapper.writeValueAsString(
-                UserForm(
-                    email = "${random}@google.com",
-                    username = "jiji",
-                    password = "1234"
-                )
-            )
-
-        val json = MockMultipartFile(
-            "user-data",
-            "jsonData",
-            MediaType.APPLICATION_JSON_VALUE,
-            content.toByteArray()
-        )
-
-        return when (hasImage) {
-            true -> multipart("/api/users")
-                .file(imageFile!!)
-                .file(json)
-                .contentType(MediaType.MULTIPART_MIXED_VALUE)
-                .characterEncoding("UTF-8")
-            false -> multipart("/api/users")
-                .file(json)
-                .contentType(MediaType.MULTIPART_MIXED_VALUE)
-                .characterEncoding("UTF-8")
-        }
+            //then
+            .andExpect(status().isNoContent)
     }
 
     data class UserResponseDto(
