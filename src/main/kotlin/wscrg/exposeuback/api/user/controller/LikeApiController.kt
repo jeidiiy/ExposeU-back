37ac73e50449e5a0fb585cfe72e99b65fc7debad
wscrg <wldyd2356@gmail.com>
2022-03-17 13:30:03 +0900

    feat: 좋아요 취소 기능 로직 및 테스트 코드 개발, API 경로 수정
    * "/api/like" -> "/api/likes" (단수 -> 복수 형태로 변경)

--- src/main/kotlin/wscrg/exposeuback/api/user/controller/LikeApiController.kt
+++ src/main/kotlin/wscrg/exposeuback/api/user/controller/LikeApiController.kt
@@ -3,6 +3,7 @@ package wscrg.exposeuback.api.user.controller
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController
 import wscrg.exposeuback.domain.dto.authentication.UserDto
 import wscrg.exposeuback.service.LikeService
 
-@RequestMapping("/api/like")
+@RequestMapping("/api/likes")
 @RestController
 class LikeApiController(
     val likeService: LikeService
@@ -22,6 +23,18 @@ class LikeApiController(
     ): ResponseEntity<String> {
         if (user != null) {
             likeService.addLike(user, imageId ?: -1L)
+            return ResponseEntity(HttpStatus.OK)
+        }
+        return ResponseEntity(HttpStatus.UNAUTHORIZED)
+    }
+
+    @DeleteMapping("/{imageId}")
+    fun subLike(
+        @AuthenticationPrincipal user: UserDto?,
+        @PathVariable imageId: Long?,
+    ): ResponseEntity<String> {
+        if (user != null) {
+            likeService.subLike(user, imageId ?: -1L)
             return ResponseEntity(HttpStatus.OK)
         }
         return ResponseEntity(HttpStatus.UNAUTHORIZED)
