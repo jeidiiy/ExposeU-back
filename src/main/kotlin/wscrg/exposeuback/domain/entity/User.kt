9abf7ef6257478d83163135e1bef3e816dc4557f
wscrg <wldyd2356@gmail.com>
2022-03-17 13:24:53 +0900

    feat: 각 엔티티에 equals(), hashCode() 오버라이딩

--- src/main/kotlin/wscrg/exposeuback/domain/entity/User.kt
+++ src/main/kotlin/wscrg/exposeuback/domain/entity/User.kt
@@ -85,4 +85,22 @@ class User private constructor(
     override fun toString(): String {
         return "User(id=$id, email='$email', username='$username', image=$image, phoneNumber=$phoneNumber)"
     }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as User
+
+        if (id != other.id) return false
+        if (email != other.email) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = id?.hashCode() ?: 0
+        result = 31 * result + email.hashCode()
+        return result
+    }
 }
